4a12c1bc9c070737e15bee59272fa9789f5f73e5
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-31 23:15:09 -0300

    Changing theme of the calculator to click on icons

--- main.js
+++ main.js
@@ -8,18 +8,47 @@ function calculateValues() {
     document.querySelector("input").value = eval(inputNumbers.replace("÷", "\/"))
 }
 
-/* function percentageValue(num){
-    insertValues(num)
-} */
-
-const btnInvertValue = document.querySelectorAll("button")[1]
+const btnInvertValue = document.querySelector(".fraction")
 btnInvertValue.addEventListener("click", () => {
     calculateValues()
     const inputNumbers = document.querySelector("input").value
     document.querySelector("input").value = inputNumbers * -1
 })
 
-const btnClearInput = document.querySelectorAll("button")[0]
+const btnClearInput = document.querySelector(".clear")
 btnClearInput.addEventListener("click", () => {
     document.querySelector("input").value = ""
 })
+
+const inputNumbers = document.querySelector("input")
+const calculator = document.querySelector("section")
+const buttonsCalculator = document.querySelectorAll(".item")
+const iconsChangeTheme = document.querySelector(".icons")
+
+const iconLightMode = document.querySelector(".icon-light-mode")
+iconLightMode.addEventListener("click", changeLightMode)
+function changeLightMode() {
+    inputNumbers.classList.remove("darkMode")
+    calculator.classList.remove("darkMode")
+    iconsChangeTheme.classList.remove("darkMode")
+    iconLightMode.classList.remove("active")
+    iconDarkMode.classList.remove("active")
+
+    for(let button of buttonsCalculator){
+        button.classList.remove("darkMode")    
+    }
+}
+
+const iconDarkMode = document.querySelector(".icon-dark-mode")
+iconDarkMode.addEventListener("click", changeDarkMode)
+function changeDarkMode() {
+    inputNumbers.classList.add("darkMode")
+    calculator.classList.add("darkMode")
+    iconsChangeTheme.classList.add("darkMode")
+    iconLightMode.classList.add("active")
+    iconDarkMode.classList.add("active")
+
+    for(let button of buttonsCalculator){
+        button.classList.add("darkMode")    
+    }
+}
